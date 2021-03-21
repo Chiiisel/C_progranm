d8a360000b74a35dd11c2340b7ee75cd4c2f7ce7
cHiiisel <chisel_112@outlook.com>
2021-03-21 23:20:50 +0800

    add dynamiclist

---
+++ dynamic-array-list-Chiiisel/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include "list.h"
+#include "list.c"
+int main()
+{
+	/* */
+	struct list *l;
+	l = create_list(10);
+
+	for (int i = 0; i < 10; i++)
+		l = insert(l, i, 'A'+i);
+	printf("struct list = %s\r\n",l->ele);
+
+	l = delete_at(l, 4);
+	printf("deleted list = %s\r\n",l->ele);
+/* other test code */
+	int the_size=0;
+	the_size = size(l);
+
+	T test;
+	test = get( l, 2);
+	printf("function get(l,2) = %c\r\n",test);
+
+	replace( l, 1, 'Z' );
+	printf("function replace(l,1,'Z') = %s\r\n",l->ele);
+
+	int the_index=0;
+	the_index = index( l, 'Z' );
+	printf("the_size = %d\r\nthe_index = %d",the_size,the_index);
+	free(l);
+}
